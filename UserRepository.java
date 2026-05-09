41f46a61e32fd35ba3a2607258775e855e191611
sakamotohifumi123-eng <sakamotohifumi123@gmail.com>
2026-05-09 20:31:40 +0900

    Create UserRepository.java

---
+++ UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.blog.repository;
+
+import com.example.blog.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository
+        extends JpaRepository<User, Integer> {
+
+    User findByUserId(Integer userId);
+
+}
